1c4d160a63ec19a0909834b439a0bdc5dd54f000
chefdude99 <chefdude.1999@gmail.com>
2025-02-19 23:31:55 +0000

    Lista la parte del HTML Y CSS, solo falta terminar el js !!

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  function mezcladorDeCarta(numeros, simbolos) {
+    let valoresRandom = Math.floor(Math.random() * numeros.length);
+    let iconosRandom = Math.floor(Math.random() * simbolos.length);
+    return { valores: valoresRandom, iconos: iconosRandom }
+  }
+  function cartaFinal (){
+    let valores = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    let iconos = ["♦", "♥", "♠", "♣"];
+  }
 };
